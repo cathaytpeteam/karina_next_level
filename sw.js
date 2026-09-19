1a77c49804a2fd0a019183b21e330c548db8cec6
kiki2023good <kiki2023good@gmail.com>
2026-09-20 04:18:29 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const C = "locha-v2";
+const FILES = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(C).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== C).map(x => caches.delete(x)))));
+  self.clients.claim();
+});
+// 有網路時優先取最新版，沒網路時用快取
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(r => { const copy = r.clone(); caches.open(C).then(c => c.put(e.request, copy)); return r; })
+      .catch(() => caches.match(e.request))
+  );
+});
