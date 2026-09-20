4ea1167a8532dc8d7208c986f8caf5fb43041f0a
cathaytpeteam <kiki2023good@gmail.com>
2026-09-21 03:20:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX="find-pax-";
-const CACHE="find-pax-release-20260921-ios-android-keyboard-fix";
+const CACHE="find-pax-release-20260921-keyboard-workspace-fix";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png","./icon-maskable-192.png","./icon-maskable-512.png","./phone-bottom-icon.png","./disrupted-pax-icon-v2.png","./scenario-icon-1.png","./scenario-icon-2.png","./scenario-icon-3.png","./scenario-icon-4.png"];
 
 self.addEventListener("install",e=>e.waitUntil(
