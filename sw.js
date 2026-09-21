2070ff0d633519c9acaf73daf3049a1fb328732e
cathaytpeteam <kiki2023good@gmail.com>
2026-09-22 04:22:04 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const CACHE_PREFIX="find-pax-";
 const APP_VERSION="v1.0";
-const CACHE=CACHE_PREFIX+APP_VERSION;
+const CACHE_REV="r2";
+const CACHE=CACHE_PREFIX+APP_VERSION+"-"+CACHE_REV;
 const PHONE_LIB="https://cdn.jsdelivr.net/npm/libphonenumber-js@1.12.29/bundle/libphonenumber-max.js";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png","./icon-maskable-192.png","./icon-maskable-512.png","./phone-bottom-icon.png","./disrupted-pax-icon-v2.png","./scenario-icon-1.png","./scenario-icon-2.png","./scenario-icon-3.png","./scenario-icon-4.png"];
 
