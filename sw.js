7e743c4c7edac297351ccd4b2ec28aaadaa0beb9
cathaytpeteam <kiki2023good@gmail.com>
2026-09-26 02:52:59 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX="find-pax-";
 const APP_VERSION="v1.1";
-const CACHE_REV="R1.1";
+const CACHE_REV="R1.1-ui-preview-6";
 const CACHE=CACHE_PREFIX+APP_VERSION+"-"+CACHE_REV;
 const ASSETS=[
   "./",
